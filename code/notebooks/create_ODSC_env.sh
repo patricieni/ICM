abc89e36393afcbbd2fdc2cf98589ee3dc31e11a
Paul Pop <paul@neurolabs.eu>
2018-09-16 16:57:38 +0100

    correct source for R

--- code/notebooks/create_ODSC_env.sh
+++ code/notebooks/create_ODSC_env.sh
@@ -1,10 +1,12 @@
-conda create --name ODSC python=3.6
-source activate ODSC
+conda create --name ODSC python=3.6 -y
+conda activate ODSC
+conda config --remove channels conda-forge
 conda update -n base conda -y
-conda install ipykernel
-conda install numpy scikit-learn pandas scipy seaborn matplotlib 
-conda install -c r r-essentials
-conda install -c mittner r-mice
-conda install -c conda-forge r-rknn
-conda install -c r rpy2
+conda install ipykernel -y
+conda install numpy scikit-learn pandas scipy seaborn matplotlib -y 
+conda install -c r r-digest r-essentials -y
+conda install -c mittner r-mice -y
+conda install -c conda-forge r-rknn -y
+conda install -c r rpy2 -y
+conda install -c conda-forge missingno -y
 
